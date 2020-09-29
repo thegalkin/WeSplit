b076f4c2cbe52ce9e5c7fe2cb5d28760d3123700
Nik Galkin <53794828+thegalkin@users.noreply.github.com>
2020-09-30 01:52:52 +0300

    Initial Commit

---
+++ First App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  First App
+//
+//  Created by Никита Галкин on 30.09.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ First App/First_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  First_AppApp.swift
+//  First App
+//
+//  Created by Никита Галкин on 30.09.2020.
+//
+
+import SwiftUI
+
+@main
+struct First_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
